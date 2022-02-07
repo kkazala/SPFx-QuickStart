6cf325131e0b7b4fc6453b7fbe95fee8a97ea408
Kinga Kazala <kinga.kazala@hotmail.com>
2022-02-07 10:24:51 +0100

    SPFx project initialized

---
+++ src/webparts/quickStart/QuickStartWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'QuickStartWebPartStrings';
+import QuickStart from './components/QuickStart';
+import { IQuickStartProps } from './components/IQuickStartProps';
+
+export interface IQuickStartWebPartProps {
+  description: string;
+}
+
+export default class QuickStartWebPart extends BaseClientSideWebPart<IQuickStartWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IQuickStartProps> = React.createElement(
+      QuickStart,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/quickStart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IQuickStartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'QuickStartWebPartStrings' {
+  const strings: IQuickStartWebPartStrings;
+  export = strings;
+}
